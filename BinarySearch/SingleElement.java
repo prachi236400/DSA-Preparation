b90a975ea4a6696ac1ea89888b953b4df4086ea9
Prachi Singh <151606991+prachi236400@users.noreply.github.com>
2026-02-27 18:35:06 +0000

    learn how to find single element in a sorted array by using binary search

---
+++ BinarySearch/SingleElement.java
@@ -0,0 +1,25 @@
+public class SingleElement{
+    public static int findSingle(int[] arr){
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+                return arr[mid];
+            }
+            if((mid%2==0  && arr[mid]==arr[mid+1] )|| (mid%2!=0 && arr[mid] ==arr[mid-1])){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr={1,1,2,2,3,3,4,5,5};
+        int res=findSingle(arr);
+        System.out.println(res);
+    }
+}
