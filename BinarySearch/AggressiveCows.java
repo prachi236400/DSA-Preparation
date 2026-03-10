ca9d4b24f49c6f9e815c2cfaee7de09a237bbdec
Prachi Singh <151606991+prachi236400@users.noreply.github.com>
2026-03-10 14:09:45 +0000

    learn how to solve the agressive cows dsa question by using binary search

---
+++ BinarySearch/AggressiveCows.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class AggressiveCows {
+    public static int cowsproblem(int[] arr, int k){
+        Arrays.sort(arr);
+        int low=1;
+        int high=arr[arr.length-1]-arr[0];
+        int ans=1;
+        
+        while(low<=high){
+            int count=1;
+            int mid=(low+high)/2;
+            int last=arr[0];
+            for(int i=0;i<arr.length;i++){
+            if(arr[i]-last>=mid){
+                count++;
+                last=arr[i];
+
+
+            }
+        }
+        if(count>=k){
+            ans=mid;
+            low=mid+1;
+            
+        }
+        else high=mid-1;
+
+    }
+        return ans;
+    
+}
+    public static void main(String[] args){
+        int[] arr={0,3,4,7,10,9};
+        int k=4;
+        int res=cowsproblem(arr,k);
+        System.out.println(res);
+    }
+    
+}
---
+++ BinarySearch/NthRoot.java
@@ -0,0 +1,34 @@
+public class NthRoot {
+    public  static int findNthRoot(int n,int m){
+        int low=0;
+        int high=m;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            int ans=1;
+            for(int i=0;i<n;i++){
+                ans=ans*mid;
+                if(ans>m){
+                    break;
+                }
+            }
+            if(ans==m) return mid;
+                
+            if(ans<m) low=mid+1;
+                
+            else high=mid-1;
+                
+
+        }
+
+        return -1;
+        
+    }
+    public static void main(String[] args){
+        int n=3;
+        int m=27;
+        int res=findNthRoot(n,m);
+        System.out.println(res);
+    }
+    
+}
