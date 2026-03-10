df945a746409b3fe36693971992b00d14beaa21c
Prachi Singh <151606991+prachi236400@users.noreply.github.com>
2026-03-10 15:33:42 +0000

    learn how to solve book allocation problem using binary search

---
+++ BinarySearch/BookAllocation.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+public class BookAllocation {
+    public static int minPages(int[] arr,int m){
+        int low=Arrays.stream(arr).min().getAsInt();
+        int high=Arrays.stream(arr).sum();
+        int ans=0;
+        while(low<=high){
+            int stu=0;
+            int count=1;
+            int mid=(low+high)/2;
+            
+            for(int i=0;i<arr.length;i++){
+                if(stu+arr[i]<=mid){
+                    stu=stu+arr[i];
+                }
+                else{
+                    count++;
+                    stu=arr[i];
+                }
+            }
+            if(count>m){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args){
+        int[] arr={12,34,67,90};
+        int m=2;
+        int res=minPages(arr,m);
+        System.out.println(res);
+
+    }
+}
