c3ef6f014cfb5f9dbab4dea09485448bcf795e67
Prachi Singh <151606991+prachi236400@users.noreply.github.com>
2026-04-02 07:18:13 +0000

    implemented : DLL (convert array to ll , print ,deletion -> at head, at tail, at positionk, node)

--- LinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList.java
@@ -61,14 +61,61 @@ public class DoublyLinkedList {
         
         return head;
     }
+    public static Node deleteAtK(Node head,int k){
+        if(head==null || head.next==null) return null;
+        Node temp=head;
+        int count=0;
+        while(temp.next!=null){
+            count++;
+            if(count==k){
+                
+                // temp.back=null;
+                // temp.next=null;
+                temp.back.next=temp.next;
+                temp.next.back=temp.back;
+                
+                
+
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+    public static Node deleteNode(Node head, int k){
+        if(head==null) return null;
+        Node temp=head;
+        for(int i=1;i<k;i++){
+            temp=temp.next;
+        }
+        if(temp==null) return null;
+        if(temp.next!=null){
+            temp.next.back=temp.back;
+
+        }
+
+        if(temp.back!=null){
+            temp.back.next=temp.next;
+        }
+        else{
+            head=temp.next;
+        }
+ 
+        return head;
+    }
 
     public static void main(String[] args){
         int[] arr={3,5,2,6};
+        int k=2;
         Node head=convertArrToAll(arr);
+        //Node newNode=new Node(arr[2]);
         print(head);
         // head=deleteHead(head);
         // print(head);
-        head=deleteTail(head);
+        // head=deleteTail(head);
+        // print(head);
+        // head=deleteAtK(head,k);
+        // print(head);
+        head = deleteNode(head,k);
         print(head);
 
 
