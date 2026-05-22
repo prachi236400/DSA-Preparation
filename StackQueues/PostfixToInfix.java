773ed4b7ffe61b4928617cb32028a633c976f948
Prachi Singh <151606991+prachi236400@users.noreply.github.com>
2026-05-22 23:40:45 +0000

    Implemented Postifx to infix conversion using stack

---
+++ StackQueues/PostfixToInfix.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class PostfixToInfix{
+    public static String postfixToInfix(String exp){
+        Stack<String> st=new Stack<>();
+        int n=exp.length();
+        for(int i=0;i<n;i++){
+            char ch=exp.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                st.push(ch+"");
+            }   
+            else{
+                String t1=st.peek();
+                st.pop();
+                String t2=st.peek();
+                st.pop();
+                String store="(" + t2 + ch +t1 +")";
+                st.push(store);
+            }
+        
+
+        }
+        return st.peek();
+    }
+    public static void main(String[] args) {
+        String exp="AB-DE+F*/";
+        System.out.println("INFIX EXP: "+ postfixToInfix(exp));
+    }
+}
